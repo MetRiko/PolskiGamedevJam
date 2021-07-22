2c77bbfb812db983232aace16edf35076741f4e5
MetRiko <18maximus8050@gmail.com>
2021-07-22 16:30:37 +0200

    [WIP] Update jump technique behaviour

--- src/Utils.gd
+++ src/Utils.gd
@@ -1,2 +1,15 @@
 extends Node
 
+func getRandomElementsFromArray(array : Array, maxAmount : int):
+	
+	if maxAmount >= array.size():
+		return array.duplicate()
+	
+	var base = array.duplicate()
+	var randomized = []
+	for i in range(maxAmount):
+		var randId = randi() % base.size()
+		randomized.append(base[randId])
+		base.remove(randId)
+	
+	return randomized
--- src/bending/JumpTechnique.gd
+++ src/bending/JumpTechnique.gd
@@ -9,61 +9,139 @@ var cellsForJump = []
 
 #var inactiveCells = []
 
-func _ready():
-	bendingCtrl.connect("attract_mode_changed", self, "onAttractModeChanged")
+var shouldUpdateCellsForJump := false
 
-func onAttractModeChanged(state):
-	if state == false:
+func _ready():
+#	bendingCtrl.connect("attract_mode_changed", self, "onAttractModeChanged")
+
+	for attractor in $Attractors.get_children():
+		attractor.connect("body_entered", self, "onCellAttractorEntered")
+		
+	for attractor in $Attractors2.get_children():
+		attractor.connect("body_entered", self, "onCellAttractorEntered")
+
+func onCellAttractorEntered(body):
+	shouldUpdateCellsForJump = true
+#	print('x')
+
+#func onAttractModeChanged(state):
+#	if state == false:
+#		for cell in cellsForJump:
+#			cell.enableGravity()
+#			cell.resetDamp()
+#			cell.changeColor(0)
+#
+#		cellsForJump = []
+
+func _updateCellsForJump():
+	var allCellsFromGround = getCellsFromAttractors()
+	var cellsLeftToAttract = clamp(20 - cellsForJump.size(), 0, 20)
+	var cells = Utils.getRandomElementsFromArray(allCellsFromGround, cellsLeftToAttract)
+	cellsForJump.append_array(cells)
+#	var cells = allCellsFromGround
+#	cellsForJump = cells
+	for cell in cells:
+		cell.disableGravity()
+		cell.linear_damp = 8.0
+		cell.changeColor(2)
+
+func _clearCellsForJump():
+	if cellsForJump.empty() == false:
 		for cell in cellsForJump:
 			cell.enableGravity()
 			cell.resetDamp()
 			cell.changeColor(0)
-		
 		cellsForJump = []
 
 func _process(delta):
-	if player.isOnFloor == false and bendingCtrl.attractMode == true:
-		enableJumpMode()
+	_updateAttractorsPos()
+	
+	if player.isOnFloor == false:
+		if shouldUpdateCellsForJump == true:
+			shouldUpdateCellsForJump = false
+			_updateCellsForJump()
 	else:
-		disableJumpMode()
+		_clearCellsForJump()
+	
+#	if player.isOnFloor == false:# and bendingCtrl.attractMode == true:
+#		enableJumpMode()
+#	else:
+#		disableJumpMode()
+
+
+func _updateAttractorsPos():
+	var space2d = get_world_2d().direct_space_state
+	
+	var startPos = player.global_position + Vector2.DOWN * 24.0
+	
+	var attractorsCount = $Attractors.get_child_count()
+	
+	for attractor in $Attractors.get_children():
+		var n = attractor.get_index() - floor(attractorsCount / 2)
+		
+		var vec = Vector2.DOWN.rotated(0.3 * n) * 240.0
+		var result = space2d.intersect_ray(startPos, startPos + vec, [], 1)
+		
+		if result:
+			var finalPos = result.position + Vector2.UP.normalized() * 10.0
+			var finalPos2 = finalPos - vec.normalized() * 20.0 
+			attractor.global_position = finalPos
+			$Attractors2.get_child(attractor.get_index()).global_position = finalPos2
+	
+#	for attractor in$Attractors2.get_children():
+#		var n = attractor.get_index() - floor(attractorsCount / 2)
+#
+#		var vec = Vector2.DOWN.rotated(0.2 * n) * 240.0
+#		var result = space2d.intersect_ray(startPos, startPos + vec, [], 1)
+#
+#		var finalPos = result.position + Vector2.UP.normalized() * 60.0
+#		attractor.global_position = finalPos
+#
+	
 
 func _physics_process(delta):
-	if bendingCtrl.attractMode == true:
-		var targetPos = player.global_position + Vector2(0.0, 24.0)
-		for cell in cellsForJump:
-			var vec = targetPos - cell.global_position
-			var vel = vec.normalized() * clamp(vec.length_squared(), 0.0, 80.0)
-			cell.impulse(vel)
-	
-		if Input.is_action_just_pressed("jump"):
-			if cellsForJump.size() > 0:
+#	if bendingCtrl.attractMode == true:
+	var targetPos = player.global_position + Vector2(0.0, 24.0)
+	for cell in cellsForJump:
+		var vec = targetPos - cell.global_position
+		var vel = vec.normalized() * clamp(vec.length_squared(), 0.0, 120.0)
+		cell.impulse(vel)
+
+	if Input.is_action_just_pressed("jump"):
+		if cellsForJump.size() > 0:
+			
+			var power = (cellsForJump.size() / 20.0)
+			
+			for cell in cellsForJump:
+				cell.enableGravity()
+				cell.resetDamp()
 				
-				var power = (cellsForJump.size() / 20.0)
+				var vec = cell.global_position - player.global_position
+				var vel = vec.normalized().rotated(rand_range(-0.2, 0.2)) * rand_range(30.0, 40.0)
 				
-				for cell in cellsForJump:
-					cell.enableGravity()
-					cell.resetDamp()
-					
-					var vec = cell.global_position - player.global_position
-					var vel = vec.normalized().rotated(rand_range(-0.2, 0.2)) * rand_range(30.0, 40.0)
-					
-					cell.linear_velocity = vel * 10.0
+				cell.linear_velocity = vel * 10.0
 #					cell.contact_monitor = true
 #					cell.contacts_reported = 3
 #					cell.connect("body_entered", self, "onCellCollision", [cell])
-					var timer = get_tree().create_timer(0.2)
-					timer.connect("timeout", self, "onCellTimeout", [cell])
-					
+				var timer = get_tree().create_timer(0.2)
+				timer.connect("timeout", self, "onCellTimeout", [cell])
 				
+			
 #				inactiveCells = cellsForJump
-				var jumpPower = 140.0
-				if player.linear_velocity.y < 0.0:
-					player.linear_velocity.y *= 0.5
-				player.impulse(Vector2.UP * jumpPower *  pow(power + 0.6, 1.5))
+			var jumpPower = 140.0
+#			if player.linear_velocity.y < 0.0:
+#				player.linear_velocity.y *= 0.5
+
+			var dir = player.linear_velocity
+			dir.y = -abs(dir.y)
+			dir = (dir + Vector2.UP * 10.0).normalized()
+			player.linear_velocity = dir * jumpPower *  pow(power + 0.8, 1.3)
+#			player.impulse(dir * jumpPower *  pow(power + 0.8, 1.3))
+#			player.impulse(Vector2.UP * jumpPower *  pow(power + 0.8, 1.3))
 #				player.jump(jumpPower * pow(power + 0., 2.0), false)
-				
-				cellsForJump = []
-				reEnableJumpMode()
+			
+			cellsForJump = []
+			reEnableJumpMode()
 
 func onCellTimeout(cell):
 	returnCellToNormal(cell)
@@ -87,10 +165,30 @@ func reEnableJumpMode():
 		jumpMode = false
 		enableJumpMode()
 
+func getCellsFromAttractors():
+	
+	var cells = []
+	for attractor in $Attractors.get_children():
+		cells.append_array(attractor.get_overlapping_bodies())
+		
+	for attractor in $Attractors2.get_children():
+		cells.append_array(attractor.get_overlapping_bodies())
+	
+	var finalCells = {}
+	for cell in cells:
+		if cell.getColorId() == 0:
+			finalCells[cell.get_instance_id()] = cell
+	
+	return finalCells.values()
+
 func enableJumpMode():
 	if jumpMode == false:
 		jumpMode = true
-		var cells = bendingCtrl.detachRandomCells(20)
+#		var cells = bendingCtrl.detachRandomCells(20)
+		var allCellsFromGround = getCellsFromAttractors()
+#		var cells = Utils.getRandomElementsFromArray(allCellsFromGround, 20)
+		var cells = allCellsFromGround
+		print(cells)
 		cellsForJump = cells
 		for cell in cellsForJump:
 			cell.disableGravity()
@@ -104,5 +202,5 @@ func disableJumpMode():
 			cell.enableGravity()
 			cell.resetDamp()
 			cell.changeColor(0)
-			bendingCtrl.addCellToAttracted(cell, true)
+#			bendingCtrl.addCellToAttracted(cell, true)
 		cellsForJump = []
